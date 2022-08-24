b57bd3dbd119ea2875e2503c933c46fb18f8d23b
Eras242 <sol@erascollective.com>
2022-08-24 02:46:17 +0100

    updates

--- Projects/listener.js
+++ Projects/listener.js
@@ -1,5 +1,6 @@
 import chalk from "chalk";
 import { ethers } from "ethers";
+import ERC20ABI from "../Abi/ERC20ABI.json" assert { type: "json" };
 const INFURA_KEY = "62dc490feac442098dcb4fc1ecaa2dc0";
 
 const main = async function () {
@@ -8,8 +9,8 @@ const main = async function () {
   );
 
   const provider = new ethers.providers.WebSocketProvider(
-    `wss://ropsten.infura.io/ws/v3/${INFURA_KEY}`,
-    "ropsten"
+    `wss://mainnet.infura.io/ws/v3/${INFURA_KEY}`,
+    "mainnet"
   );
 
   const network = provider
@@ -20,16 +21,36 @@ const main = async function () {
       )
     );
 
+  // const iface = new ethers.utils.Interface(ERC20ABI);
+  // const tx = await provider.getTransaction(
+  //   "0x8df3112f885f5816b0c668ab8a4f7bdce5f9615ae82cbe6100b60cfd4ae68fdf"
+  // );
+  // const dcTx = iface.parseTransaction(tx);
+  // const token = new ethers.Contract(tx.to, ERC20ABI, provider);
+  // const tokenName = await token.name();
+  // console.log(tokenName);
+  const transferSig = "0xa9059cbb";
 
   const pendingTxs = () => {
-    provider.on("pending", (tx) => {
-      console.log(
-        `[  ${new Date().toLocaleTimeString()}  ]  New Transaction: ${tx}`
-      );
+    provider.on("pending", async (transaction) => {
+      if (transaction) {
+        const tx = await provider.getTransaction(transaction);
+        if (tx)
+          if (tx.data.indexOf(transferSig) !== -1) {
+            const token = new ethers.Contract(tx.to, ERC20ABI, provider);
+            const symbol = await token.symbol();
+            console.log(
+              ` [ ${chalk.bgBlue(" TRANSFER ")} ] ${transaction.slice(
+                0,
+                6
+              )} ${symbol} `
+            );
+          }
+      }
     });
   };
 
-  // pendingTxs();
+  pendingTxs();
 };
 
 main();
